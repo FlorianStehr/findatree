0cd2e26e369528e1a6445e55b4d2286927acfbfe
FlorianStehr <florian.stehr@gmail.com>
2022-06-28 13:49:37 +0200

    interactive plotting class working

--- findatree/interactive.py
+++ findatree/interactive.py
@@ -1,6 +1,6 @@
 from turtle import width
 from typing import Dict, List, Tuple
-from matplotlib.pyplot import colorbar
+from matplotlib.pyplot import colorbar, figimage
 import numpy as np
 import importlib
 
@@ -10,6 +10,7 @@ import bokeh.layouts
 
 from bokeh.plotting.figure import Figure
 from bokeh.models import Column
+from bokeh.models import Toggle
 
 import findatree.transformations as transformations
 importlib.reload(transformations)
@@ -22,11 +23,12 @@ class Plotter:
         self.channels = None
         self.segments = None
         self.source = bokeh.plotting.ColumnDataSource(data={})
-        self.figures = []
+        self.figures = {}
         
         # Style attributes
         self.size = 400
         self.palette = 'Magma256'
+        self.title_font_size = '150%'
         self.nan_color = 'rgba(0, 0, 0, 0)'
         self.bounds_color_mapper = bokeh.transform.LinearColorMapper(
             palette='Greys8',
@@ -48,9 +50,6 @@ class Plotter:
         data['y'] = [params_channels['shape'][0] * params_channels['px_width']]
         data['width'] = [params_channels['shape'][1] * params_channels['px_width']]
         data['height'] = [params_channels['shape'][0] * params_channels['px_width']]
-
-        # Add empty (i.e. NaN values) segment bounds to source
-        data['bounds'] = [np.ones(params_channels['shape'], dtype=np.float32) * np.nan]
 
         # Update
         self.source = bokeh.plotting.ColumnDataSource(data=data)
@@ -85,7 +84,13 @@ class Plotter:
         # Get source data
         data = self.source.data
 
-        data[channel_name] = [self.channels[channel_name].copy()]
+        try:
+            data[channel_name] = [self.channels[channel_name].copy()]
+        except:
+            try:
+                data[channel_name] = [self.segments[channel_name].copy()]
+            except:
+                raise KeyError(f"`{channel_name}` not in self.channels nor in self.segments.")
 
         # Update
         self.source = bokeh.plotting.ColumnDataSource(data=data)
@@ -119,10 +124,12 @@ class Plotter:
 
         # Create figure
         fig = bokeh.plotting.figure(
-            width=self.size,
-            height=self.size,
-            x_range=self.x_range,
-            y_range=self.y_range,
+            width = self.size,
+            height = self.size,
+            x_range = self.x_range,
+            y_range = self.y_range,
+            x_axis_label='x [m]',
+            y_axis_label='y [m]',
             active_scroll='wheel_zoom',
             )
 
@@ -133,7 +140,7 @@ class Plotter:
         return fig
 
 #%%
-    def figures_add_rgb(self, perc:float = 1) -> Column:
+    def figures_add_rgb(self, perc:float = 1):
         
         # Add figure
         fig = self._figure_create()
@@ -157,36 +164,14 @@ class Plotter:
         )
         fig.add_tools(hover_tool)
 
-        # Add bounds
-        bounds = fig.image(
-            source=self.source,
-            image='bounds',
-            x='x',
-            y='y',
-            dw='width',
-            dh='height',
-            color_mapper=self.bounds_color_mapper,
-            visible=False,
-        )
-
-        # Add toggler for showing bounds
-        toggler = bokeh.models.Toggle(
-            label="Segments",
-            button_type="success",
-            width=self.size // 2,
-            )
-        toggler.js_link('active', bounds, 'visible')
-
-        # Combine figure and toggler to column 
-        col = bokeh.layouts.column([fig, toggler])
-
         # Update
-        self.figures.extend([col])
+        self.figures['rgb'] = [fig]
+
+        pass
         
-        return col
 
 #%%
-    def figures_add_gray(self, channel_name: str, perc: float = 1) -> Column:
+    def figures_add_gray(self, channel_name: str, perc: float = 0.5):
         
         # Add figure
         fig = self._figure_create()
@@ -198,7 +183,7 @@ class Plotter:
         vmin = np.nanpercentile(self.source.data[channel_name][0], perc)
         vmax = np.nanpercentile(self.source.data[channel_name][0], 100 - perc)
         color_mapper = bokeh.transform.LinearColorMapper(palette=self.palette , low=vmin , high=vmax, nan_color=self.nan_color)
-
+        
         # Add gray scale image to figure
         img_rgba = fig.image(
             source=self.source,
@@ -213,7 +198,6 @@ class Plotter:
         # Add hover tool
         hover_tool = bokeh.models.HoverTool(
             tooltips=[
-                ("(x, y)", "($x, $y)"),
                 (channel_name,f"@{channel_name}"),
             ],
         )
@@ -230,130 +214,77 @@ class Plotter:
         )
         slider.js_link('value',color_mapper,'low', attr_selector=0)
         slider.js_link('value',color_mapper,'high', attr_selector=1)
-
-        # Combine figure and slider to column 
-        col = bokeh.layouts.column([fig, slider])
         
         # Update
-        self.figures.extend([col])
+        self.figures[channel_name] = [fig, slider]
         
-        return col
+        pass
 
+#%%
+    def _figures_add_toggler_bounds(self) -> Toggle:
+        
+        # Assert that bounds are in source
+        assert 'bounds' in self.source.data.keys(), "`bounds` not in self.source.data"
+            
+        # Add toggler for showing bounds
+        toggler = bokeh.models.Toggle(
+            active = True, 
+            label = "Show Segments",
+            button_type = "success",
+            width = self.size // 4,
+            height = self.size // 8,
+            )
+        
+        # Loop through all figures and add bounds image connected to toggler
+        for key in self.figures:
+            fig = self.figures[key][0]
+
+            # Add bounds
+            bounds = fig.image(
+                source=self.source,
+                image='bounds',
+                x='x',
+                y='y',
+                dw='width',
+                dh='height',
+                color_mapper=self.bounds_color_mapper,
+                visible=True,
+            )
+            toggler.js_link('active', bounds, 'visible')
+
+        return toggler
+
+
+#%%
     def create_layout(self):
+        
+        # Create list of bokeh columns by combining all elements in self.figures and add title division
+        cols = []
+        for key in self.figures:
+            # Add title division
+            div = bokeh.models.Div(
+                text=f"{key}".upper(),
+                style={'font-size': self.title_font_size},
+                width=self.size,
+                height=self.size // 16,
+                )
+
+            # Define column elements as list 
+            col_elements = [div]
+            for item in self.figures[key]:
+                col_elements.extend([item])
+
+            # Create column and add to columns  
+            col = bokeh.layouts.column(col_elements)
+            cols.extend([col])
 
         # Create final layout
-        layout = bokeh.layouts.layout([self.figures])
+        try:
+            toggler = self._figures_add_toggler_bounds()
+            cols.extend([toggler])
+        except:
+            pass
+
+        layout = bokeh.layouts.layout([cols])
         
         return layout 
-
-
-
-####################################################################################################
-####################################################################################################
-####################################################################################################
-
-# import bokeh.plotting
-# import bokeh.models
-# import bokeh.transform
-# import bokeh.layouts
-
-# importlib.reload(interact)
-
-# fig_rgba = interact.bk_fig_img_rgb(cs['RGB'])
-
-# shape = params_cs['shape']
-# data = {
-#         'chm': [cs['chm'].copy()],
-#         'ndvi': [cs['ndvi'].copy()],
-#         'lightness': [cs['l'].copy()],
-#         'x': [0],
-#         'y': [shape[0]],
-#         'dw': [shape[1]],
-#         'dh': [shape[0]],
-# }
-# source = bokeh.plotting.ColumnDataSource(data=data)
-
-# panels = []
-# for key in ['chm', 'ndvi', 'lightness']:
-#     # Figure
-#     fig = bokeh.plotting.figure(
-#             x_range=fig_rgba.x_range,
-#             y_range=fig_rgba.y_range,
-#             active_scroll='wheel_zoom',
-#     )
-
-#     # Add colormapper
-#     p = 0
-#     vmin = np.nanpercentile(source.data[key],p)
-#     vmax = np.nanpercentile(source.data[key],100 - p)
-#     mapper = bokeh.transform.LinearColorMapper(palette='Greys256' , low=vmin , high=vmax)
-
-#     # Add image
-#     img = fig.image(
-#         source=source,
-#         image=key,
-#         x='x',
-#         y='y',
-#         dw='dw',
-#         dh='dh',
-#         color_mapper=mapper,
-#     )
-
-#     # Add slider
-#     slider = bokeh.models.RangeSlider(start=vmin, end=vmax, value=(vmin,vmax), step=(vmax-vmin)/20, title=f"{key} range:")
-#     slider.js_link('value',mapper,'low', attr_selector=0)
-#     slider.js_link('value',mapper,'high', attr_selector=1)
-
-#     # Layout
-#     col = bokeh.layouts.column([fig, slider])
-#     panel = bokeh.models.Panel(child=col, title=key)
-#     panels.extend([panel])
-
-# tabs = bokeh.models.Tabs(tabs=panels, tabs_location = 'right')
-# rows = bokeh.layouts.row([fig_rgba, tabs])
-
-# show(rows)
-
-####################################################################################################
-####################################################################################################
-####################################################################################################
-
-# from bokeh.layouts import layout
-# from bokeh.transform import linear_cmap
-# from bokeh.models import RangeSlider, Slider
-
-
-# p1 = figure()
-# img_rgba = p1.image_rgba(
-#     image=[rgba_ui32],
-#     x=[0],
-#     y=[0],
-#     dw=[shape[0]],
-#     dh=[shape[1]],
-# )
-
-# p2 = figure(x_range=p1.x_range, y_range=p1.y_range)
-
-# rslider_chm = RangeSlider(start=0, end=50, value=(5,30), step=1, title='CHM height')
-# color_mapper = LinearColorMapper(palette="Greys256", low=0, high=40)
-# rslider_chm.js_link('value',color_mapper,'low',attr_selector=0)
-# rslider_chm.js_link('value',color_mapper,'high',attr_selector=1)
-
-# p2.image(
-#     image=[cs['chm']],
-#     x=[0],
-#     y=[0],
-#     dw=[shape[0]],
-#     dh=[shape[1]],
-#     color_mapper=color_mapper,
-# )
-
-# lo = layout([
-#     [rslider_chm],
-#     [
-#         [p1],
-#         [p2]
-#     ]
-# ])
-
-# show(lo)
