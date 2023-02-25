feee37362e7248f579d3984ba015cf25968dd51b
FlorianStehr <florian.stehr@gmail.com>
2023-02-25 23:57:47 +0100

    Everything seems to be working again, transition finished

--- setup.py
+++ setup.py
@@ -1,16 +1,2 @@
-from setuptools import setup
-
-setup(
-   name='findatree',
-   version='0.1.1',
-   description='Detect trees in drone data',
-   author='Stehr Florian',
-   author_email='floraian.stehr@gmail.com',
-   url="https://github.com/FlorianStehr/findatree",
-   packages=['findatree'],
-   classifiers=[
-        "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent",
-   ],
-   install_requires=[],
-)
+import setuptools
+setuptools.setup()
